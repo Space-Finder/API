7a9e9710d4e682d78c8789ed914c72b5f6d5deea
FusionSid <siddheshadsv@icloud.com>
2024-08-20 14:42:05 +1200

    feat: add route to check if user is teacher

--- src/routes/teacher.py
+++ src/routes/teacher.py
@@ -1,6 +1,7 @@
 __all__ = ("teacher_router",)
 
-from fastapi import APIRouter
+import validators
+from fastapi import APIRouter, Request
 
 teacher_router = APIRouter(
     tags=[
@@ -8,3 +9,19 @@ teacher_router = APIRouter(
     ],
     prefix="/api/teachers",
 )
+
+SCHOOL_DOMAIN = "ormiston.school.nz"
+TEACHERS = ["st22209@ormiston.school.nz", "hprasad@ormiston.school.nz"]
+
+
+@teacher_router.get("/verify")
+async def verify_is_teacher(request: Request, email: str):
+    # Will be updated to use database, once data is given from Mr Lambert
+    conditions = [
+        email,
+        validators.email(email),
+        email.endswith(SCHOOL_DOMAIN),
+        email in TEACHERS,
+    ]
+
+    return all(conditions)
