984b0708a4a3404b9a245cad3cbad98d2e0a8843
FusionSid <siddheshadsv@icloud.com>
2024-08-29 10:52:39 +1200

    feat: fetch teachers list from db instead of hardcoded

--- src/routes/teacher.py
+++ src/routes/teacher.py
@@ -2,6 +2,7 @@ __all__ = ("teacher_router",)
 
 import validators
 from fastapi import APIRouter, Request
+from core import prisma
 
 teacher_router = APIRouter(
     tags=[
@@ -11,17 +12,17 @@ teacher_router = APIRouter(
 )
 
 SCHOOL_DOMAIN = "ormiston.school.nz"
-TEACHERS = ["gsheppard@ormiston.school.nz", "hprasad@ormiston.school.nz"]
 
 
 @teacher_router.get("/verify")
 async def verify_is_teacher(request: Request, email: str):
-    # Will be updated to use database, once data is given from Mr Lambert
+    teachers_list = {teacher.email for teacher in await prisma.teacher.find_many()}
+
     conditions = [
         email,
         validators.email(email),
         email.endswith(SCHOOL_DOMAIN),
-        email in TEACHERS,
+        email in teachers_list,
     ]
 
     return {"success": True, "isTeacher": all(conditions)}
