7987de6abb56c76d99ab323bd52bc8ae8c2b368b
FusionSid <siddheshadsv@icloud.com>
2024-09-01 19:04:04 +1200

    feat: add booking and space routers to routers list

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -4,6 +4,15 @@ from .other import other_router
 from .teacher import teacher_router
 from .course import course_router
 from .image import image_router
+from .space import space_router
+from .booking import booking_router
 
-router_list = [other_router, teacher_router, course_router, image_router]
+router_list = [
+    other_router,
+    teacher_router,
+    course_router,
+    image_router,
+    space_router,
+    booking_router,
+]
 middleware_list = []
