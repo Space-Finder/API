7dc38def708640d4aed041db7dfadafcc452b918
FusionSid <siddheshadsv@icloud.com>
2024-09-02 00:58:05 +1200

    feat: add route to get list of classes

--- src/routes/course.py
+++ src/routes/course.py
@@ -1,6 +1,8 @@
 __all__ = ("course_router",)
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+
+from core import prisma
 
 course_router = APIRouter(
     tags=[
@@ -8,3 +10,14 @@ course_router = APIRouter(
     ],
     prefix="/api/courses",
 )
+
+
+@course_router.get("/")
+async def get_classes(request: Request, teacher_id: str):
+    classes = await prisma.course.find_many(
+        where={"teacherId": teacher_id},
+    )
+    if not classes:
+        raise HTTPException(status_code=404, detail="No classes found for this teacher")
+
+    return classes
